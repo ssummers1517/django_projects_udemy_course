c36a2ab36417ccf835aeaac8541b1115e802e1a6
Steven Summers <ssummers1517@gmail.com>
2022-06-15 05:58:01 -0600

    Created a .env file to store my Django secret key. The .env file is on my .gitignore.

--- food_menu/food_menu/settings.py
+++ food_menu/food_menu/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*g-d_utbt4#)q%-i5u5=ot!l=df_m8ua0oew@w87w(a(=#fy)0'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
